72a5f6bad3bec92f4296f95603d91dae57ffbb01
Wagner Trezub <wagner.trezub@redturtle.it>
2025-03-27 15:36:30 +0100

    chore: removed console.log

--- src/actions/toggleBlocksPreview.js
+++ src/actions/toggleBlocksPreview.js
@@ -7,7 +7,6 @@ export const TOGGLE_BLOCKS_PREVIEW = 'TOGGLE_BLOCKS_PREVIEW';
  * @returns {Object} toggle action.
  */
 export function setToggleBlocksPreview(value) {
-  console.log('qui entra');
   return {
     type: TOGGLE_BLOCKS_PREVIEW,
     toggle: value,
